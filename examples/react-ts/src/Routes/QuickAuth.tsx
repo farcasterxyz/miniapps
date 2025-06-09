31356f53a6e39d222bfb7f0087a488462da01239
Tony D'Addeo <amdaddeo@gmail.com>
2025-06-09 16:26:37 -0500

    clean

--- examples/react-ts/src/Routes/QuickAuth.tsx
+++ examples/react-ts/src/Routes/QuickAuth.tsx
@@ -5,32 +5,36 @@ import { preconnect } from 'react-dom'
 function QuickAuth() {
   preconnect('https://auth.farcaster.xyz')
 
+  return (
+    <>
+      <h1>sdk.quickAuth</h1>
+      <GetToken />
+      <div style={{ marginTop: 10 }}>
+        <Fetch />
+      </div>
+    </>
+  )
+}
+
+function GetToken() {
   const [token, setToken] = useState<string>()
-  const [user, setUser] = useState<any>()
 
   const getToken = useCallback(async () => {
     const { token } = await sdk.quickAuth.getToken()
     setToken(token)
   }, [])
 
-  const fetch = useCallback(async () => {
-    const res = await sdk.quickAuth.fetch('http://localhost:8787/me')
-    if (res.ok) {
-      setUser(await res.json())
-    }
-  }, [])
-
   return (
-    <>
-      <h1>sdk.quickAuth</h1>
+    <div>
       <button type="button" onClick={getToken}>
         getToken
       </button>
-      {!!token && <code>{token}</code>}
-      <div style={{ marginTop: 10 }}>
-        <Fetch />
-      </div>
-    </>
+      {!!token && (
+        <div>
+          <code>{token}</code>
+        </div>
+      )}
+    </div>
   )
 }
 
